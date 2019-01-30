06f5bfc12d4b9c949573027196f3974a3d2c5ad3
mstfssmn <46896122+mstfssmn@users.noreply.github.com>
2019-01-30 10:39:07 +0200

    siren cms poc 2 first commit

---
+++ SirencmsClient/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './_Components/nav/nav.component';
+import { FooterComponent } from './_Components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
